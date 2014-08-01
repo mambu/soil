d9974a1d982adf7a21f7a722e636c488f1f7a6b2
Marco Ambu <marco.ambu@gmail.com>
2014-08-01 10:54:24 +0100

    Added definition for numeric types of fixed size (wrapper of types defined in cstdint)

---
+++ include/NumType.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   NumType.h
+ * Author: marco
+ */
+
+#ifndef SOIL_NUMTYPE_H
+#define	SOIL_NUMTYPE_H
+
+#include <cstdint>
+
+namespace soil
+{
+
+typedef std::int8_t  Int8;
+typedef std::uint8_t UInt8;
+
+typedef std::int16_t  Int16;
+typedef std::uint16_t UInt16;
+
+typedef std::int32_t  Int32;
+typedef std::uint32_t UInt32;
+
+typedef std::int64_t  Int64;
+typedef std::uint64_t UInt64;
+
+} // namespace soil
+
+#endif // SOIL_NUMTYPE_H
+
