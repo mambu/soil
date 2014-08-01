e41d223b45695355c1647e5c8263cf98a0ab122c
Marco Ambu <marco.ambu@gmail.com>
2014-08-01 14:14:26 +0100

    Implemented append to file in FileLogWriter

--- src/FileLogWriter.cpp
+++ src/FileLogWriter.cpp
@@ -4,11 +4,14 @@ namespace soil
 {
 
 FileLogWriter::FileLogWriter(const std::string& filename, bool append)
-    : mFile(filename.c_str())
+    : mFile(filename.c_str(), append
+                              ? std::ios_base::out | std::ios_base::app
+                              : std::ios_base::out)
 {
     if (!mFile.is_open())
     {
         // TODO: handle error
     }
 }
-}
+
+} // namespace soil
